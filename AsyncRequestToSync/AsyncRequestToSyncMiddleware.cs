9c02eb5d84ff621be540e17cf01b6c0e02db5a6a
Mohammad Khazaei <khazaei.m7@gmail.com>
2023-08-03 20:16:14 +0100

    add middleware

---
+++ AsyncRequestToSync/AsyncRequestToSyncMiddleware.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Http;
+
+namespace AsyncRequestToSync
+{
+    public class AsyncRequestToSyncMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AsyncRequestToSyncMiddleware(RequestDelegate next) => _next = next;
+
+        public async Task InvokeAsync(HttpContext context, AsyncConectionHandler conectionHandler)
+        {
+            await _next(context);
+
+            // Hnadle only 202 responses
+            if (context.Response.StatusCode != 202)
+                return;
+            if (!context.Response.Headers.TryGetValue("CorrelationId", out var correlationIdValues))
+                return;
+            var correlationId = new Guid(correlationIdValues.First());
+            await conectionHandler.WaitForResponse(context, correlationId);
+        }
+    }
+}
